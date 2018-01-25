3d963ad549d65c13d49e31a962c8c934a87f1d59
Joshua <joshua.jungen@outlook.de>
2018-01-25 14:55:34 +0100

    Use thread pool in main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 extern crate project_analyser;
 
 use project_analyser::downloader;
+use project_analyser::thread_helper::ThreadPool;
 
 fn main() {
     project_analyser::setup_logger().expect("Could not setup logger");
+
     let projects = downloader::read_project_urls_from_file();
 
-    for project in &projects[5..10] {
-        downloader::clone_project(project);
+    let thread_pool = ThreadPool::new(5);
+    for project in projects.into_iter() {
+        thread_pool.execute(move || {
+            downloader::clone_project(&project);
+        })
     }
 
-    //downloader::clone_project(&downloader::GitHubProject::new(0, "urlllll".to_string()));
+    println!("Shutting down");
+
 }
