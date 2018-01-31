194c2180d74f6c089b55006e83a3ccdb51327c1e
Joshua <joshua.jungen@outlook.de>
2018-01-31 09:45:53 +0100

    Moved mod database to folder

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -17,7 +17,7 @@ pub fn establish_connection() -> PgConnection {
     PgConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
-
+ /*
 pub fn show_posts() {
     use super::schema::github_projects::dsl::*;
 
@@ -34,17 +34,20 @@ pub fn show_posts() {
         println!("{}", post.url);
     }
 }
+*/
+
 pub fn insert_new_project(project: &NewGitHubProject) -> Result<GitHubProject, ErrorKind> {
     use schema::github_projects;
     let connection = establish_connection();
     let inserted_project:GitHubProject = match diesel::insert_into(github_projects::table)
         .values(project)
         .get_result(&connection) {
-            Ok(x) => x,
-            Err(_) => return Err(ErrorKind::AlreadyExists),
-        };
+        Ok(x) => x,
+        Err(_) => return Err(ErrorKind::AlreadyExists),
+    };
     Ok(inserted_project)
 }
+
 pub fn get_project_by_url(url_str: String) -> Result<GitHubProject, ErrorKind> {
     use super::schema::github_projects::dsl::*;
 
@@ -52,7 +55,30 @@ pub fn get_project_by_url(url_str: String) -> Result<GitHubProject, ErrorKind> {
     match github_projects
         .filter(url.eq(url_str))
         .first(&connection) {
-            Ok(x) => Ok(x),
-            Err(_) => Err(ErrorKind::NotFound)
-        }
+        Ok(x) => Ok(x),
+        Err(_) => Err(ErrorKind::NotFound)
+    }
+}
+
+mod commit_frequency {
+
+}
+
+mod git_repository {
+
+    pub fn create() {
+
+    }
+
+    pub fn read() {
+
+    }
+
+    pub fn update() {
+
+    }
+
+    pub fn delete() {
+
+    }
 }
