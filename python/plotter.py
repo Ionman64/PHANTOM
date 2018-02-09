b1e8c8fa73a0687e5ce914a79d4f6ef128fcad3f
Joshua <joshua.jungen@outlook.de>
2018-02-09 19:55:32 +0100

    Add peak and right shift

--- python/plotter.py
+++ python/plotter.py
@@ -23,20 +23,39 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def plot_line(series, key, ax, rolling_mean=True):
+def populate_figure(group, ax_line, ax_norm, ax_acc, ax_acc_norm, style_line="-", style_rolling_mean='--',
+                    window_size_rolling_mean=5, ):
+    group.plot(label=key, style=style_line, legend=True, ax=ax_line)
+    rolling_mean_for_series(group).plot(label=key, style=style_rolling_mean, color=last_color(ax_line), ax=ax_line)
+
+    norm_group = normalise_series(group)
+    norm_group.plot(label=key, style=style_line, legend=True, ax=ax_norm)
+    rolling_mean_for_series(norm_group).plot(label=key, style=style_rolling_mean, color=last_color(ax_norm), ax=ax_norm)
+
+    acc_group = accumulate_series(group)
+    acc_group.plot(label=key, style=style_line, legend=True, ax=ax_acc)
+    norm_acc_group = normalise_series(accumulate_series(group))
+    norm_acc_group.plot(label=key, style=style_line, legend=True, ax=ax_acc_norm)
+
+
+def last_color(axes):
+    return axes.lines[-1].get_color()
+
+
+def plot_line(series, key, ax, rolling_mean=True, window_size=5):
     series.plot(label=key, style='-', legend=True, ax=ax)
-    if rolling_mean:
-        series.rolling(window=5).mean().plot(color=ax.lines[-1].get_color(), style='--', ax=ax)
 
 
-def plot_line_normalised(series, key, ax, rolling_mean=True):
-    normed = (series - series.min()) / (series.max() - series.min())
-    plot_line(normed, key, ax, rolling_mean)
+def rolling_mean_for_series(series, window_size=5):
+    return series.rolling(window=window_size).mean()
 
 
-def plot_line_accumulated(series, key, ax):
-    acc = series.agg(np.add.accumulate)
-    plot_line_normalised(acc, key, ax, rolling_mean=False)
+def accumulate_series(series):
+    return series.agg(np.add.accumulate)
+
+
+def normalise_series(series):
+    return (series - series.min()) / (series.max() - series.min())
 
 
 if __name__ == '__main__':
@@ -74,33 +93,70 @@ if __name__ == '__main__':
         con=engine,
         index_col='commit_date')
 
-    fig1, ax1 = plt.subplots(3, sharex=True)
-    fig2, ax2 = plt.subplots(3, sharex=True)
-    ax = {'line': ax1[0], 'norm': ax1[1], 'acc': ax1[2], 'left-line': ax2[0], 'left-norm': ax2[1], 'left-acc': ax2[2]}
+    fig_dates = plt.figure(figsize=(10, 10))
+    fig_leftshift = plt.figure(figsize=(10, 10))
+    fig_rightshift = plt.figure(figsize=(10, 10))
+    fig_max_peak = plt.figure(figsize=(10, 10))
+    ax = {
+        'line': plt.subplot2grid((3, 2), (0, 0), colspan=2, fig=fig_dates),
+        'norm': plt.subplot2grid((3, 2), (1, 0), colspan=2, fig=fig_dates),
+        'acc': plt.subplot2grid((3, 2), (2, 0), colspan=1, fig=fig_dates),
+        'acc-norm': plt.subplot2grid((3, 2), (2, 1), colspan=1, fig=fig_dates),
+        'left-line': plt.subplot2grid((3, 2), (0, 0), colspan=2, fig=fig_leftshift),
+        'left-norm': plt.subplot2grid((3, 2), (1, 0), colspan=2, fig=fig_leftshift),
+        'left-acc': plt.subplot2grid((3, 2), (2, 0), colspan=1, fig=fig_leftshift),
+        'left-acc-norm': plt.subplot2grid((3, 2), (2, 1), colspan=1, fig=fig_leftshift),
+        'right-line': plt.subplot2grid((3, 2), (0, 0), colspan=2, fig=fig_rightshift),
+        'right-norm': plt.subplot2grid((3, 2), (1, 0), colspan=2, fig=fig_rightshift),
+        'right-acc': plt.subplot2grid((3, 2), (2, 0), colspan=1, fig=fig_rightshift),
+        'right-acc-norm': plt.subplot2grid((3, 2), (2, 1), colspan=1, fig=fig_rightshift),
+        'max-peak-line': plt.subplot2grid((3, 2), (0, 0), colspan=2, fig=fig_max_peak),
+        'max-peak-norm': plt.subplot2grid((3, 2), (1, 0), colspan=2, fig=fig_max_peak),
+        'max-peak-acc': plt.subplot2grid((3, 2), (2, 0), colspan=1, fig=fig_max_peak),
+        'max-peak-acc-norm': plt.subplot2grid((3, 2), (2, 1), colspan=1, fig=fig_max_peak),
+    }
     ax['line'].set_ylabel("frequency")
-    ax['norm'].set_ylabel("norm. frequency")
-    ax['acc'].set_ylabel("acc. frequency")
+    ax['norm'].set_ylabel("norm.")
+    ax['acc'].set_ylabel("acc.")
+    ax['acc-norm'].set_ylabel("acc. norm.")
+    #
     ax['left-line'].set_ylabel("frequency")
-    ax['left-norm'].set_ylabel("norm. frequency")
-    ax['left-acc'].set_ylabel("acc. frequency")
-    fig1.suptitle("Commit frequency over time")
-    fig2.suptitle("Commit frequency over time delta")
+    ax['left-norm'].set_ylabel("norm.")
+    ax['left-acc'].set_ylabel("acc.")
+    ax['left-acc-norm'].set_ylabel("acc.")
+    #
+    ax['right-line'].set_ylabel("frequency")
+    ax['right-norm'].set_ylabel("norm.")
+    ax['right-acc'].set_ylabel("acc.")
+    ax['right-acc-norm'].set_ylabel("acc.")
+    #
+    ax['max-peak-line'].set_ylabel("frequency")
+    ax['max-peak-norm'].set_ylabel("norm.")
+    ax['max-peak-acc'].set_ylabel("acc.")
+    ax['max-peak-acc-norm'].set_ylabel("acc. norm.")
+    fig_dates.suptitle("Commit frequency")
+    fig_leftshift.suptitle("Commit frequency (left shifted)")
+    fig_rightshift.suptitle("Commit frequency (right shifted)")
+    fig_max_peak.suptitle("Commit frequency (max peak shifted)")
 
-    # frame.groupby('repository_id')['frequency']:
-
+    style_line = '-'
+    style_rolling_mean = '--'
     for key, group in frame.groupby('repository_id'):
-        ## Plot over date
         group = group.frequency.resample(arg_time_unit).sum()
+        populate_figure(group, ax_line=ax['line'], ax_norm=ax['norm'], ax_acc=ax['acc'], ax_acc_norm=ax['acc-norm'])
 
-        plot_line(group, key, ax['line'])
-        plot_line_normalised(group, key, ax['norm'])
-        plot_line_accumulated(group, key, ax['acc'])
-        ## Plot over time delta
         leftshifted_x = [(group.index[idx] - group.index[0]).days for idx in range(len(group.index[:-1]))]
         leftshifted = pd.Series(data=group.values[:-1], index=leftshifted_x)
+        populate_figure(leftshifted, ax_line=ax['left-line'], ax_norm=ax['left-norm'], ax_acc=ax['left-acc'], ax_acc_norm=ax['left-acc-norm'])
 
-        plot_line(leftshifted, key, ax['left-line'])
-        plot_line_normalised(leftshifted, key, ax['left-norm'])
-        plot_line_accumulated(leftshifted, key, ax['left-acc'])
+        rightshifted_x = [leftshifted_x[idx] - leftshifted_x[-1] for idx in range(len(leftshifted_x))]
+        rightshifted = pd.Series(data=group.values[:-1], index=rightshifted_x)
+        populate_figure(rightshifted, ax_line=ax['right-line'], ax_norm=ax['right-norm'], ax_acc=ax['right-acc'], ax_acc_norm=ax['right-acc-norm'])
+
+        max_peak = np.argmax(group.values)
+        max_peakshifted_x = [leftshifted_x[idx] - leftshifted_x[max_peak] for idx in range(len(leftshifted_x))]
+        max_peakshifted = pd.Series(data=group.values[:-1], index=max_peakshifted_x)
+        populate_figure(max_peakshifted, ax_line=ax['max-peak-line'], ax_norm=ax['max-peak-norm'], ax_acc=ax['max-peak-acc'], ax_acc_norm=ax['max-peak-acc-norm'])
+
 
     plt.show()
