047de099d7bbd101e2b978cba042541369dae470
Joshua <joshua.jungen@outlook.de>
2018-04-28 18:34:28 +0200

    Enquote all fields on git log generation

--- scripts/save_git_log.sh
+++ scripts/save_git_log.sh
@@ -2,6 +2,6 @@
 path_to_git=$1
 output_file=$2
 
-git --git-dir $path_to_git/.git log --format='%H,%P,%an,%ae,%at,%cn,%ce,%ct' > $output_file
+git --git-dir $path_to_git/.git log --format='"%H","%P","%an","%ae","%at","%cn","%ce","%ct"' > $output_file
 
 rm -r $path_to_git
